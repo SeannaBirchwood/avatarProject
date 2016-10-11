a8e1ec9fc78a08ab51f612d9029581cf1e0cfb4a
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-11 00:51:43 +0000

    Collecting notes and relevent URLs, and re-arranging some basic code and pseudo code

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,44 @@
 var request = require('request');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, endpoint, cb) {
+  //
+	var repoOwner = ""
+	var repoName = ""
+	var endpoint = repoOwner + "/" repoName;
+	var options = {
+    url: `${githubRoot}${endpoint}`,
+    headers: {
+      'User-Agent': 'request'
 
+  var uriString = "https://api.github.com/v3/repos/".split("") + endpoint + "contributors".split("");
+    }
+  };
+
+  // Just pass the callback down to request
+  request.get(options, callback);
 }
+
+} //We will need code here to filter the avatar data
+
+response.on(data, function() {
+	console.log("Received", data.length)
+})
 
 getRepoContributors("lighthouse-labs", "laser_shark", (err, result) => {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+getRepoContributors()
+
+//next we need code to produce img files on the local disk from the information
+// retrieved above
+
+
+
+"https://api.github.com/repos/lighthouse-labs/laser_shark/collaborators{/collaborator}"
+
+"https://api.github.com/repos/octocat/Hello-World"
+
+in the morning, research quickly the frameworks behind "request library" and
+the reasons behind streaming. 
