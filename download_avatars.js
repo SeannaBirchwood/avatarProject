fa4f735cce75870c4f600bf1cd7ad3b3d93e76d9
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-12 19:54:41 +0000

    Managed to break code. Readjusted and saved, and now it's cleaner and still works.

--- download_avatars.js
+++ download_avatars.js
@@ -4,8 +4,6 @@ var http = require("http");
 
 var sourceArgs = process.argv.slice(2);
 
-console.log(sourceArgs);
-
 var options = {
   url: "https://api.github.com/repos/" + sourceArgs[0] + "/" + sourceArgs[1] + "/contributors",
   headers: {
@@ -16,34 +14,23 @@ var options = {
 
 function getRepoContributors(repoOwner, repoName, _callback) {
 //find contributors, which comes into an array
-  request.get(options, function(err, data) {
-
+request.get(options, function(err, data) {
   var contribUrls = JSON.parse(data.body);
   for (var i = 0; i < contribUrls.length; i++) {
     downloadImageByURL(contribUrls[i]["avatar_url"], "avatar_images/" + contribUrls[i]["login"] + ".png");
-      }
-
-    });
+  }
+});
 };
 
 function downloadImageByURL(url, filePath) {
-  request(url).pipe(fs.createWriteStream(filePath)).on('close', function() {
-    return ""
-  });
+//got this from stack -- research why it works
+request(url).pipe(fs.createWriteStream(filePath)).on('close', function() {
+  return ""
+});
 };
 
-// getRepoContributors("lighthouse-labs", "laser_shark", (err, result) => {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-// });
-
-
-
-
-//https://api.github.com/repos/lighthouse-labs/laser_shark/contributors
-
-
-// in the morning, research quickly the frameworks behind "request library" and
-// the reasons behind streaming. 
-
-//egghead.io might be useful - video instruction
+//error / debug tool
+getRepoContributors("lighthouse-labs", "laser_shark", (err, result) => {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
